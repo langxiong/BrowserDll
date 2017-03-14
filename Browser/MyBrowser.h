956d505019fe2ac2a1e5753d4b0691e51b3d9d29
hwt <hwt@xlfy.tech>
2017-03-14 06:50:54 +0000

    Update 'Browser/MyBrowser.h'

--- Browser/MyBrowser.h
+++ Browser/MyBrowser.h
@@ -71,7 +71,7 @@ extern "C"
             {
                 if (params->rgvarg[i].vt == VT_BSTR)
                 {
-                    ::MessageBox(NULL, params->rgvarg[0].bstrVal, _T("js call c++"), MB_OK);
+                    ::MessageBox(NULL, params->rgvarg[i].bstrVal, _T("js call c++"), MB_OK);
                 }
             }
             if (result)
